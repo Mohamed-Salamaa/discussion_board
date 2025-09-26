c1814736d03fdd58025fae267bc6d3e752da7867
Mohamed-Salamaa <mohamedsalamaa41@gmail.com>
2025-09-26 07:16:27 +0300

    initial commit

---
+++ boards/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=150)
+
+class Topic(models.Model):
+    subject = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, related_name="topics", on_delete=models.CASCADE)
+    creation = models.DateTimeField(auto_now_add=True)
+    board = models.ForeignKey(Board, related_name="topics", on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    message = models.CharField(max_length=4000)
+    created_by = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
+    creation = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, related_name="posts", on_delete=models.CASCADE)
