8d526274125e4e249c9ffe9fe257fd2609a03b41
Mohamed-Salamaa <mohamedsalamaa41@gmail.com>
2025-09-27 15:43:04 +0300

    WIP discussion board

--- boards/models.py
+++ boards/models.py
@@ -6,15 +6,23 @@ class Board(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.name
+
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     created_by = models.ForeignKey(User, related_name="topics", on_delete=models.CASCADE)
     creation = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, related_name="topics", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.subject
 
 class Post(models.Model):
     message = models.CharField(max_length=4000)
     created_by = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
     creation = models.DateTimeField(auto_now_add=True)
     topic = models.ForeignKey(Topic, related_name="posts", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.message
--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Board, Topic, Post
+from .forms import NewTopicForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def home(request):
+    context = {}
+    boards = Board.objects.all()
+    context["boards"] = boards
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def board_topics(request, board_id):
+    context = {"board": Board.objects.get(pk=board_id)}
+    return render(request, 'topics.html', context)
+
+
+def new_topic(request, board_id):
+    form = NewTopicForm()
+    board = Board.objects.get(pk=board_id)
+    user = User.objects.first()
+    if request.method == "POST":
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.created_by = user
+            topic.save()
+
+            Post.objects.create(
+                message=form.cleaned_data.get("message"),
+                created_by=user,
+                topic=topic
+            )
+            return redirect('board_topics', board_id=board.pk)
+    else:
+        form = NewTopicForm()
+
+    return render(request, 'new_topic.html', {"board": board, 'form': form})
